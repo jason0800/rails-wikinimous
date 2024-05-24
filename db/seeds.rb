a81a4567373ee1bdc9f080eda16c6f4b8020a120
Jason Wang <jasonwang332@gmail.com>
2024-05-24 17:47:48 +1000

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+3.times { Article.create(title: Faker::Games::Pokemon.name, content: Faker::Games::Pokemon.move) }
